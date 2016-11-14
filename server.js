5addf3333414f4e6e4dade250638d19488f9d77a
Gerken <cgerken@ms.ds.uhc.com>
2016-11-14 12:54:13 -0600

    Change port from 3000 to 80

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.post('/search', function (req, res) {
   });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(80);
 
-console.log('Listening on port: ' + (process.env.PORT || 3000));
+console.log('Listening on port: ' + (80);
